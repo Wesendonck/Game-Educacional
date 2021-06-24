29b2577464976752a41bf36f6128fbef46738080
Lucas Wesendonck <lucas.wesendonck@hotmail.com>
2021-06-24 14:59:53 -0300

    Inclusão das bibliotecas utilizadas + criação do registro de texto

---
+++ jogo.py
@@ -0,0 +1,8 @@
+import pygame  # Game Desenvolvido por: Lucas Wesendonck
+import random
+import time
+file = open('registro.txt', 'w+')
+file.write(input('Digite seu Nome: '))
+file.write('\n ')
+file.write(input('Digite seu e-mail: '))
+file.close()
