b27fbaa7c4347e757b19aa94c6836104fbe4e1c9
Lucas Wesendonck <lucas.wesendonck@hotmail.com>
2021-06-24 15:06:28 -0300

    Criação de variáveis para inserção das imagens

--- jogo.py
+++ jogo.py
@@ -6,3 +6,24 @@ file.write(input('Digite seu Nome: '))
 file.write('\n ')
 file.write(input('Digite seu e-mail: '))
 file.close()
+
+pygame.init()
+largura = 800
+altura = 660
+icone = pygame.image.load("imagens/lixoicone.png")
+pygame.display.set_caption("Reciclagem do Alemão")
+pygame.display.set_icon(icone)
+tela = pygame.display.set_mode((largura, altura))
+fps = pygame.time.Clock()
+
+background = pygame.image.load('imagens/01.jpg')  # plano de fundo
+lixolata = pygame.image.load('imagens/lixolata.png')
+lixovidro = pygame.image.load('imagens/lixovidro.png')
+lixoplastico = pygame.image.load('imagens/lixoplastico.png')
+lixopapel = pygame.image.load('imagens/lixopapel.png')
+lata = pygame.image.load('imagens/lata.png')
+plastico = pygame.image.load('imagens/plastico.png')
+papel = pygame.image.load('imagens/papel.png')
+vidro = pygame.image.load('imagens/vidro.png')
+branco = (255, 255, 255)
+preto = (0, 0, 0)
